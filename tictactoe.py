63ac7e540b26c7ccb3f84993385330c7a3801fda
cmitchh <cmitchh@gmail.com>
2016-02-14 12:24:00 -0800

    Update tictactoe.py
    functional, except doesn't recognize win

--- tictactoe.py
+++ tictactoe.py
@@ -22,18 +22,18 @@ def createboards():
 
 def displayboard(A, B, C, D, E, F, G, H, I, Overall):
 	print("\n "+A[0]+" "+A[1]+" "+A[2]+"  |  "+B[0]+" "+B[1]+" "+B[2]+"  |  "+C[0]+" "+C[1]+" "+C[2])
-	print(" "+A[3]+" "+A[4]+" "+A[5]+"  |  "+B[3]+" "+B[5]+" "+B[5]+"  |  "+C[3]+" "+C[4]+" "+C[5])
-	print(" "+A[5]+" "+A[7]+" "+A[8]+"  |  "+B[6]+" "+B[7]+" "+B[8]+"  |  "+C[6]+" "+C[7]+" "+C[8])
+	print(" "+A[3]+" "+A[4]+" "+A[5]+"  |  "+B[3]+" "+B[4]+" "+B[5]+"  |  "+C[3]+" "+C[4]+" "+C[5])
+	print(" "+A[6]+" "+A[7]+" "+A[8]+"  |  "+B[6]+" "+B[7]+" "+B[8]+"  |  "+C[6]+" "+C[7]+" "+C[8])
 	print("________|_________|_______")
 	print("        |         |        ")
 	print(" "+D[0]+" "+D[1]+" "+D[2]+"  |  "+E[0]+" "+E[1]+" "+E[2]+"  |  "+F[0]+" "+F[1]+" "+F[2])
-	print(" "+D[3]+" "+D[4]+" "+D[5]+"  |  "+E[3]+" "+E[5]+" "+E[5]+"  |  "+F[3]+" "+F[4]+" "+F[5])
-	print(" "+D[5]+" "+D[7]+" "+D[8]+"  |  "+E[6]+" "+E[7]+" "+E[8]+"  |  "+F[6]+" "+F[7]+" "+F[8])
+	print(" "+D[3]+" "+D[4]+" "+D[5]+"  |  "+E[3]+" "+E[4]+" "+E[5]+"  |  "+F[3]+" "+F[4]+" "+F[5])
+	print(" "+D[6]+" "+D[7]+" "+D[8]+"  |  "+E[6]+" "+E[7]+" "+E[8]+"  |  "+F[6]+" "+F[7]+" "+F[8])
 	print("________|_________|_______")
 	print("        |         |        ")
 	print(" "+G[0]+" "+G[1]+" "+G[2]+"  |  "+H[0]+" "+H[1]+" "+H[2]+"  |  "+I[0]+" "+I[1]+" "+I[2])
-	print(" "+G[3]+" "+G[4]+" "+G[5]+"  |  "+H[3]+" "+H[5]+" "+H[5]+"  |  "+I[3]+" "+I[4]+" "+I[5])
-	print(" "+G[5]+" "+G[7]+" "+G[8]+"  |  "+H[6]+" "+H[7]+" "+H[8]+"  |  "+I[6]+" "+I[7]+" "+I[8]+"\n")
+	print(" "+G[3]+" "+G[4]+" "+G[5]+"  |  "+H[3]+" "+H[4]+" "+H[5]+"  |  "+I[3]+" "+I[4]+" "+I[5])
+	print(" "+G[6]+" "+G[7]+" "+G[8]+"  |  "+H[6]+" "+H[7]+" "+H[8]+"  |  "+I[6]+" "+I[7]+" "+I[8]+"\n")
 
 def begin():
 	board = raw_input("\nPlayer 1: Select a board to begin with, by entering a number from A to I \n>>")
@@ -128,8 +128,123 @@ def switchplayer(activeplayer):
 		mark = "x"
 	return activeplayer, mark
 
-#def checksmallboards(A, B, C, D, E, F, G, H, I):
-#	if ( )
+def checksmallboards(A, B, C, D, E, F, G, H, I, Overall, mark):
+		#this needs simplifying
+		if ((check(A) == True) and (A[9] == "open")):
+			Overall[0] == mark
+			if (mark == "x"):
+				A = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				A = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			return A, B, C, D, E, F, G, H, I, Overall
+		elif ((check(B) == True) and (B[9] == "open")):
+			if (mark == "x"):
+				B = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				B = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			Overall[1] == mark
+			return A, B, C, D, E, F, G, H, I, Overall
+		elif ((check(C) == True) and (C[9] == "open")):
+			if (mark == "x"):
+				C = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				C = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			Overall[2] == mark 
+			return A, B, C, D, E, F, G, H, I, Overall
+		elif ((check(D) == True) and (D[9] == "open")):
+			if (mark == "x"):
+				D = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				D = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			Overall[3] == mark
+			return A, B, C, D, E, F, G, H, I, Overall
+		elif ((check(E) == True) and (E[9] == "open")):
+			if (mark == "x"):
+				E = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				E = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			Overall[4] == mark
+			return A, B, C, D, E, F, G, H, I, Overall
+		elif ((check(F) == True) and (F[9] == "open")):
+			if (mark == "x"):
+				F = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				F = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			Overall[5] == mark
+			return A, B, C, D, E, F, G, H, I, Overall
+		elif ((check(G) == True) and (G[9] == "open")):
+			if (mark == "x"):
+				G = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				G = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			Overall[6] == mark
+			return A, B, C, D, E, F, G, H, I, Overall
+		elif ((check(H) == True) and (H[9] == "open")):
+			if (mark == "x"):
+				H = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				H = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			Overall[7] == mark
+			return A, B, C, D, E, F, G, H, I, Overall
+		elif ((check(I) == True) and (I[9] == "open")):
+			if (mark == "x"):
+				I = ["\\", " ", "/", " ", "X", " ", "/", " ", "\\", "won"]
+			else:
+				I = ["/", " ", "\\", "|", " ", "|", "\\", " ", "/", "won"]
+			Overall[8] == mark
+			return A, B, C, D, E, F, G, H, I, Overall
+		else:
+			return A, B, C, D, E, F, G, H, I, Overall
+
+def checkoverallboard(Overall, activeplayer):
+	state = check(Overall)
+	activeplayer = str(activeplayer)
+	if (state == True):
+		print ("Game Over\nPlayer "+activeplayer+" wins!")
+		return True
+	else:
+		return False
+
+
+def check(board):
+	if ((board[0] == board[1] == board[2]) or (board[0] == board[3] == board[6]) or (board[0] == board[4] == board[8]) or (board[1] == board[4] == board[7]) or (board[2] == board[5] == board[8]) or (board[3] == board[4] == board[5]) or (board[6] == board[7] == board[8]) or (board[2] == board[4] == board[6])):
+		return True
+	else:
+		return False
+
+
+def nextboard(A, B, C, D, E, F, G, H, I, lmove):
+	board = lmove
+	if ((board == "A") and (A[9] == "open")):
+		return board
+	elif ((board == "B") and (B[9] == "open")):
+		return board
+	elif ((board == "C") and (C[9] == "open")):
+		return board
+	elif ((board == "D") and (D[9] == "open")):
+		return board
+	elif ((board == "E") and (E[9] == "open")):
+		return board
+	elif ((board == "F") and (F[9] == "open")):
+		return board
+	elif ((board == "G") and (G[9] == "open")):
+		return board
+	elif ((board == "H") and (H[9] == "open")):
+		return board
+	elif ((board == "I") and (I[9] == "open")):
+		return board
+	else:
+		board = getnewboard(A, B, C, D, E, F, G, H, I)
+		return board
+
+		
+def getnewboard(A, B, C, D, E, F, G, H, I):
+	board = raw_input("\nSelect an open board to play on, by entering a number from A to I \n>>")
+	if ((board == "A" and A[9] == "open") or (board == "B" and B[9] == "open") or (board == "C" and C[9] == "open") or (board == "D" and D[9] == "open") or (board == "E" and E[9] == "open")or (board == "F" and F[9] == "open") or (board == "G" and G[9] == "open") or (board == "H" and H[9] == "open") or (board == "I" and I[9] == "open")):
+		return board
+	else:
+		print ("\nThat is not a valid board selection\n")
+		getnewboard()
 
 
 
@@ -138,31 +253,38 @@ def switchplayer(activeplayer):
 
 def main():
 	welcome()
+	#initialize and print boards
 	A, B, C, D, E, F, G, H, I, Overall = createboards()
 	displayboard(A, B, C, D, E, F, G, H, I, Overall)
+	#player 1 selects starting board
 	board = begin()	
 	activeplayer = 1
 	mark = "x"
 	wincondition = False
-
+	#play starts
 	while (wincondition == False):
 		move, lmove = play(board, activeplayer, mark)
 		A, B, C, D, E, F, G, H, I = updateboards(A, B, C, D, E, F, G, H, I, move, board, activeplayer, mark)
 		displayboard(A, B, C, D, E, F, G, H, I, Overall)
-		#checksmallboards(A, B, C, D, E, F, G, H, I)
-		board = lmove
+		A, B, C, D, E, F, G, H, I, Overall = checksmallboards(A, B, C, D, E, F, G, H, I, Overall, mark)
+		wincondition = checkoverallboard(Overall, activeplayer)
+		board = nextboard(A, B, C, D, E, F, G, H, I, lmove)
 		activeplayer, mark = switchplayer(activeplayer)
 	
-	print("Player %d wins!" % activeplayer)
+
 
 
 #todo: 
-#check to see if move wins a board, 
-#if move wins update overall board (check if overall board is won), 
-#check if next board is complete
+#fix delay in small board win check
+#fix overall win check
 
-#figure out why P2 first turn doesnt register
+#\   /
+#  X
+#/   \
 
+#/   \
+#|   |
+#\   /
 
 
 
